79198b2a38595418f118db001fc21598ea8f97b0
Mike Pettman <mp_palmatum@yahoo.com>
2017-09-26 13:41:06 -0500

    more styling added

--- public/scripts/controllers/sauron_controller.js
+++ public/scripts/controllers/sauron_controller.js
@@ -1,8 +1,13 @@
 myApp.controller('SauronController', function(){
     var vm = this;
-    
+    vm.message = true;
+
     vm.playSound = function(){
         var sound = new Audio('sounds/work.wav');
         sound.play();
+    };
+    
+    vm.showMessage = function(){
+        vm.message = !vm.message;
     };
 });
